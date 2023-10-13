ecbfc37bac69e6bd7d2bb9ed0d6f632c40f7f77e
Mehmoli <meagulec@gmail.com>
2023-10-13 15:40:30 +0200

    Eerste opzet cabforyou project.

---
+++ src/test/java/com/novi/cabforyou/CabforyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novi.cabforyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabforyouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
