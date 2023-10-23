99d2370510d1cbd4d4789cf450a1ad67876b6da7
Mehmoli <meagulec@gmail.com>
2023-10-23 22:31:22 +0200

    Removed boolean existById..

--- src/main/java/com/novi/cabforyou/repositories/DriverRepository.java
+++ src/main/java/com/novi/cabforyou/repositories/DriverRepository.java
@@ -3,13 +3,9 @@ package com.novi.cabforyou.repositories;
 import com.novi.cabforyou.models.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Map;
 import java.util.Optional;
 
 public interface DriverRepository extends JpaRepository<Driver, Long> {
-    void deleteByUsername(String username);
-
     Optional<Driver> findByUsername(String username);
-
-    boolean existsByUsername(String username);
+    void deleteByUsername(String username);
 }
