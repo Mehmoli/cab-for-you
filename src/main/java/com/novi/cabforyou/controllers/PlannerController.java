71e7e76d684c2ae10093044361b475497250316a
Mehmoli <meagulec@gmail.com>
2023-10-23 23:40:21 +0200

    PlannerController and PlannerService several methods added..

--- src/main/java/com/novi/cabforyou/controllers/PlannerController.java
+++ src/main/java/com/novi/cabforyou/controllers/PlannerController.java
@@ -1,4 +1,56 @@
 package com.novi.cabforyou.controllers;
 
+import com.novi.cabforyou.dtos.PlannerDto;
+import com.novi.cabforyou.services.PlannerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping(value = "/cabforyou/planners")
 public class PlannerController {
+    
+    private PlannerService plannerService;
+
+    public PlannerController(PlannerService plannerService) {
+        this.plannerService = plannerService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<PlannerDto>> getAllPlanners() {
+
+        List<PlannerDto> dtos = plannerService.getAllPlanners();
+
+        return ResponseEntity.ok(dtos);
+    }
+
+    //Is OK
+    @GetMapping("/{username}")
+    public ResponseEntity<PlannerDto> getPlanner(@PathVariable("username") String username) {
+        PlannerDto dto = plannerService.getPlanner(username);
+        return ResponseEntity.ok(dto);
+    }
+
+    //Is OK
+    @PostMapping("")
+    public ResponseEntity<PlannerDto> addPlanner(@RequestBody PlannerDto dto) {
+        PlannerDto dto1 = plannerService.addPlanner(dto);
+        return ResponseEntity.created(null).body(dto1);
+    }
+
+    //Is OK
+    @PutMapping(value = "/{username}")
+    public ResponseEntity<PlannerDto> updatePlanner(@PathVariable("username") String username, @RequestBody PlannerDto dto) {
+
+        plannerService.updatePlanner(username, dto);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    //Is OK
+    @DeleteMapping(value = "/{username}")
+    public ResponseEntity<Object> deletePlanner(@PathVariable("username") String username) {
+        plannerService.deletePlanner(username);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/novi/cabforyou/services/PlannerService.java
+++ src/main/java/com/novi/cabforyou/services/PlannerService.java
@@ -1,8 +1,15 @@
 package com.novi.cabforyou.services;
 
-
+import com.novi.cabforyou.dtos.PlannerDto;
+import com.novi.cabforyou.exceptions.UsernameNotFoundException;
+import com.novi.cabforyou.models.Planner;
 import com.novi.cabforyou.repositories.PlannerRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlannerService {
@@ -11,5 +18,78 @@ public class PlannerService {
 
     public PlannerService(PlannerRepository plannerRepository){
         this.plannerRepository = plannerRepository;
+    }
+
+    public List<PlannerDto> getAllPlanners() {
+        List<PlannerDto> plannerDto = new ArrayList<>();
+        List<Planner> planners = plannerRepository.findAll();
+        for (Planner dr: planners){
+            plannerDto.add(transferToPlannerDto(dr));
+        }
+        return plannerDto;
+    }
+
+    public PlannerDto getPlanner(String username) {
+        PlannerDto dto = new PlannerDto();
+        Optional<Planner> planner = plannerRepository.findByUsername(username);
+        if (planner.isPresent()) {
+            dto = transferToPlannerDto(planner.get());
+        } else {
+            throw new UsernameNotFoundException(username);
+        }
+        return dto;
+    }
+
+    public PlannerDto addPlanner(PlannerDto plannerDto) {
+
+        Planner planner = transferToPlanner(plannerDto);
+        plannerRepository.save(planner);
+        return plannerDto;
+    }
+
+
+    public void updatePlanner(String username, PlannerDto newPlanner) {
+        Optional<Planner> plannerOptional = plannerRepository.findByUsername(username);
+
+        if (plannerOptional.isPresent()) {
+            Planner planner = plannerOptional.get();
+            planner.setEmail(newPlanner.getEmail());
+            planner.setPlannerName(newPlanner.getPlannerName());
+            planner.setUsername(newPlanner.getUsername());
+            planner.setPassword(newPlanner.getPassword());
+
+            plannerRepository.save(planner);
+        } else {
+            throw new UsernameNotFoundException(username);
+        }
+    }
+
+    @Transactional
+    public void deletePlanner(String username) {
+        plannerRepository.deleteByUsername(username);
+    }
+
+    private PlannerDto transferToPlannerDto(Planner planner) {
+
+        var dto = new PlannerDto();
+
+        dto.username = planner.getUsername();
+        dto.password = planner.getPassword();
+        dto.email = planner.getEmail();
+        dto.plannerName = planner.getPlannerName();
+
+        return dto;
+    }
+
+    private Planner transferToPlanner(PlannerDto plannerDto) {
+
+        Planner planner = new Planner();
+
+        planner.setUsername(plannerDto.getUsername());
+        planner.setPassword(plannerDto.getPassword());
+        planner.setEmail(plannerDto.getEmail());
+        planner.setPlannerName(plannerDto.getPlannerName());
+
+        return planner;
     }
 }
