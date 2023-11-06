f248cc101a87d7c7ca7302476821caf9b9f713ef
Mehmoli <meagulec@gmail.com>
2023-11-06 18:28:24 +0100

    Relationship change.

--- src/main/java/com/novi/cabforyou/models/Planner.java
+++ src/main/java/com/novi/cabforyou/models/Planner.java
@@ -1,33 +1,75 @@
 package com.novi.cabforyou.models;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+import java.util.List;
 import java.util.Set;
 
 @Entity
 @Table(name="planners")
 public class Planner extends User {
-    private String plannerName;
+    private String employeeNumber;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "planner")
+    List<Trip> trips;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "planner")
+    List<BookingRequest> bookingRequests;
+
     public Planner() {
     }
 
-    public Planner(String username, String password, Set<Authority> authorities, String email, String firstName, String lastName, String phoneNumber, String plannerName) {
+
+
+    public Planner(String username, String password, Set<Authority> authorities, String email, String firstName, String lastName, String phoneNumber, String employeeNumber, List<Trip> trips, List<BookingRequest> bookingRequests) {
         super(username, password, authorities, email, firstName, lastName, phoneNumber);
-        this.plannerName = plannerName;
+        this.employeeNumber = employeeNumber;
+        this.trips = trips;
+        this.bookingRequests = bookingRequests;
     }
 
-    public Planner(String plannerName) {
-        this.plannerName = plannerName;
+    public Planner(String employeeNumber, List<Trip> trips, List<BookingRequest> bookingRequests) {
+        this.employeeNumber = employeeNumber;
+        this.trips = trips;
+        this.bookingRequests = bookingRequests;
     }
 
-    public String getPlannerName() {
-        return plannerName;
+    public String getEmployeeNumber() {
+        return employeeNumber;
     }
 
-    public void setPlannerName(String plannerName) {
-        this.plannerName = plannerName;
+    public void setEmployeeNumber(String employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public List<Trip> getTrips() {
+        return trips;
+    }
+
+    public void setTrips(List<Trip> trips) {
+        this.trips = trips;
+    }
+
+    public List<BookingRequest> getBookingRequests() {
+        return bookingRequests;
+    }
+
+    public void setBookingRequests(List<BookingRequest> bookingRequests) {
+        this.bookingRequests = bookingRequests;
+    }
+
+    public void addTrip(Trip tr) {
+        //Todo
+    }
+
+    public void deleteTrip(Trip trip) {
+        //Todo
     }
 }
