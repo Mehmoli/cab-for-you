83ec3d4913274b12c6813c955e7895d6ce16462d
Mehmoli <meagulec@gmail.com>
2023-11-06 18:30:56 +0100

    Code cleaning.

--- src/main/java/com/novi/cabforyou/repositories/BookingRequestRepository.java
+++ src/main/java/com/novi/cabforyou/repositories/BookingRequestRepository.java
@@ -1,11 +1,11 @@
 package com.novi.cabforyou.repositories;
 
 import com.novi.cabforyou.models.BookingRequest;
-import com.novi.cabforyou.enums.BookingStatus;
+import com.novi.cabforyou.models.BookingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface BookingRepository extends JpaRepository<BookingRequest, Long> {
+public interface BookingRequestRepository extends JpaRepository<BookingRequest, Long> {
     Optional<BookingRequest> findByBookingStatus(BookingStatus bookingStatus);
 }
