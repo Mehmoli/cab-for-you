5b7b1998a1f6dde9fe68d1c5cc0ddeabc1ce1985
Mehmoli <meagulec@gmail.com>
2023-11-11 21:51:22 +0100

    FileUpAndDownloadController and FileNotFoundException added.

--- src/main/java/com/novi/cabforyou/repositories/FileUpAndDownloadRepository.java
+++ src/main/java/com/novi/cabforyou/repositories/FileUpAndDownloadRepository.java
@@ -3,6 +3,6 @@ package com.novi.cabforyou.repositories;
 import com.novi.cabforyou.models.FileUpAndDownload;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileUpAndDownloadRepository extends JpaRepository <FileUpAndDownloadRepository, Long> {
+public interface FileUpAndDownloadRepository extends JpaRepository <FileUpAndDownload, Long> {
     FileUpAndDownload findByFileName(String fileName);
 }
