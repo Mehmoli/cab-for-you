69d2ee99cd39948117e44cd90cb0f848b8c5e057
Mehmoli <meagulec@gmail.com>
2023-11-15 09:51:34 +0100

    line 24 25 changed to @Min and @Max, line 34 feedbackOfCustomer changed to customer.

--- src/main/java/com/novi/cabforyou/models/Feedback.java
+++ src/main/java/com/novi/cabforyou/models/Feedback.java
@@ -1,7 +1,11 @@
 package com.novi.cabforyou.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.LocalDate;
 
@@ -17,18 +21,18 @@ public class Feedback {
     private String feedback;
 
     @NotNull(message = "Please give rating")
-//  @Pattern(regexp = "^((?=.*[*])(?=.*[1-5])){2}$",
-//  message = "Rating must contain atleast 1 (*) and 1 digit from 1 to 5, length should be 2")
+    @Min(value = 1, message = "Min value is 1")
+    @Max(value = 5, message = "Max value is 5")
     private Double rating;
 
     @NotNull(message = "Enter the feedback submission date")
-//	@PastOrPresent
-//	@JsonFormat(pattern = "dd/mm/yyyy")
+	@PastOrPresent
+	@JsonFormat(pattern = "dd/mm/yyyy")
     private LocalDate submitDate;
 
     @ManyToOne
     @JoinColumn(name = "customer_username")
-    private Customer feedbackOfCustomer;
+    private Customer customer;
 
     public Long getFeedbackId() {
         return feedbackId;
@@ -62,11 +66,11 @@ public class Feedback {
         this.submitDate = submitDate;
     }
 
-    public Customer getFeedbackOfCustomer() {
-        return feedbackOfCustomer;
+    public Customer getCustomer() {
+        return customer;
     }
 
-    public void setFeedbackOfCustomer(Customer feedbackOfCustomer) {
-        this.feedbackOfCustomer = feedbackOfCustomer;
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
     }
 }
