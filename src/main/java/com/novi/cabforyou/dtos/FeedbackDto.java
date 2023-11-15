928f0e7a28759d1c121f3444bfaf54ba35d9535b
Mehmoli <meagulec@gmail.com>
2023-11-15 22:54:24 +0100

    Validations removed from Feedback to FeedbackDto.

--- src/main/java/com/novi/cabforyou/dtos/FeedbackDto.java
+++ src/main/java/com/novi/cabforyou/dtos/FeedbackDto.java
@@ -1,21 +1,30 @@
 package com.novi.cabforyou.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIncludeProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.LocalDate;
 
 public class FeedbackDto {
 
     public Long feedbackId;
-
+    @NotNull(message = "Add feedback here")
     public String feedback;
-
+    @NotNull(message = "Please give rating")
+    @Min(value = 1, message = "Min value is 1")
+    @Max(value = 5, message = "Max value is 5")
     public Double rating;
-
+    @NotNull(message = "Enter the feedback submission date")
+    @PastOrPresent
+    @JsonFormat(pattern = "dd/mm/yyyy")
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonSerialize(using = LocalDateSerializer.class)
     public LocalDate submitDate;
