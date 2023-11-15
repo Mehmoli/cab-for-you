92bafe21718349908711990d0a0463e50bea7ca0
Mehmoli <meagulec@gmail.com>
2023-11-15 22:50:26 +0100

    GlobalCorsConfiguration, SpringSecurityConfig, AuthenticationController, ExceptionController, AuthenticationRequest, AuthenticationResponse, CustomUserDetailsService and JwtUtil newly added.

---
+++ src/main/java/com/novi/cabforyou/controllers/ExceptionController.java
@@ -0,0 +1,35 @@
+package com.novi.cabforyou.controllers;
+
+import com.novi.cabforyou.exceptions.BadRequestException;
+import com.novi.cabforyou.exceptions.FileNotFoundException;
+import com.novi.cabforyou.exceptions.RecordNotFoundException;
+import com.novi.cabforyou.exceptions.UsernameNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(value = RecordNotFoundException.class)
+    public ResponseEntity<Object> exception(RecordNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = FileNotFoundException.class)
+    public ResponseEntity<Object> exception(FileNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = BadRequestException.class)
+    public ResponseEntity<String> exception(BadRequestException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = UsernameNotFoundException.class)
+    public ResponseEntity<String> exception(UsernameNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+}
---
+++ src/main/java/com/novi/cabforyou/dtos/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.novi.cabforyou.dtos;
+
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+}
